2171b7e5f0037f2eb06643319186f58bad144019
dan <rylandcs@gmail.com>
2024-08-05 10:40:04 -0500

    editing routes for deleting accounts

--- service/routes.py
+++ service/routes.py
@@ -108,7 +108,20 @@ def update_account(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_account(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    
+    account.delete()  # Assuming there's a `delete()` method to remove the account from the database
+
+    return '', status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
